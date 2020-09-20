983c08f38fc3c005bd1fd861e03e3244b004cfb6
Andrey <wowand@gmail.com>
2020-09-20 06:09:01 +0300

    base tests: iOS and android web

--- src/main/java/page_objects/web_po/GoogleSite.java
+++ src/main/java/page_objects/web_po/GoogleSite.java
@@ -2,10 +2,10 @@ package page_objects.web_po;
 
 import io.appium.java_client.AppiumDriver;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
@@ -18,10 +18,13 @@ public class GoogleSite {
     private AppiumDriver appiumDriver;
 
     @FindBy(xpath = "//input[@name='q']")
-    public WebElement searchInput;
+    private WebElement searchInput;
 
     @FindBy(xpath = "//*[@id='rso']//div[@role='heading']")
-    List<WebElement> searchResults;
+    private List<WebElement> searchResults;
+
+    @FindBy(className = "sbct")
+    private List<WebElement> suggests;
 
     public GoogleSite(AppiumDriver appiumDriver) {
         this.appiumDriver = appiumDriver;
@@ -41,11 +44,21 @@ public class GoogleSite {
 
     public GoogleSite searchText(String searchText) {
         searchInput.sendKeys(searchText);
-        searchInput.sendKeys(Keys.ENTER);
+        suggests.get(0).click();
+
+        // sendKeys doesn't work for iOs devices, at least in this way.
+        // searchInput.sendKeys(Keys.ENTER);
+
+        new WebDriverWait(appiumDriver, 10).until(
+                ExpectedConditions.visibilityOf(searchResults.get(2))
+        );
         return this;
     }
 
     public List<String> getListResults() {
+        new WebDriverWait(appiumDriver, 10).until(
+                ExpectedConditions.visibilityOf(searchResults.get(2))
+        );
         return searchResults.stream()
                 .map(WebElement::getText)
                 .collect(Collectors.toList());
--- src/main/java/setup/BaseTest.java
+++ src/main/java/setup/BaseTest.java
@@ -2,7 +2,10 @@ package setup;
 
 import io.appium.java_client.AppiumDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 import page_objects.PageObject;
 
 import java.io.File;
@@ -21,12 +24,21 @@ public class BaseTest implements IDriver {
         return appiumDriver;
     }
 
-    @Parameters({"platformName", "appType", "deviceName", "browserName", "app"})
+    @Parameters({"platformName", "appType", "deviceName", "udid", "browserName", "app",
+            "appPackage", "appActivity", "bundleId"})
     @BeforeSuite(alwaysRun = true)
-    public void setUp(String platformName, String appType, String deviceName,
-                      @Optional("") String browserName, @Optional("") String app) throws Exception {
+    public void setUp(String platformName,
+                      String appType,
+                      @Optional("") String deviceName,
+                      @Optional("") String udid,
+                      @Optional("") String browserName,
+                      @Optional("") String app,
+                      @Optional("") String appPackage,
+                      @Optional("") String appActivity,
+                      @Optional("") String bundleId) throws Exception {
         System.out.println("Before: app type - " + appType);
-        setAppiumDriver(platformName, deviceName, browserName, app);
+        setAppiumDriver(platformName, deviceName, udid, browserName, app, appPackage, appActivity,
+                bundleId);
         setPageObject(appType, appiumDriver);
     }
 
@@ -36,18 +48,30 @@ public class BaseTest implements IDriver {
         appiumDriver.closeApp();
     }
 
-    private void setAppiumDriver(String platformName, String deviceName, String browserName,
-                                 String app) {
+    private void setAppiumDriver(String platformName, String deviceName, String udid,
+                                 String browserName, String app, String appPackage,
+                                 String appActivity, String bundleId) {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         //mandatory Android capabilities
         capabilities.setCapability("platformName", platformName);
         capabilities.setCapability("deviceName", deviceName);
+        capabilities.setCapability("udid", udid);
 
         if (app.endsWith(".apk")) capabilities.setCapability("app",
                 (new File(app)).getAbsolutePath());
 
         capabilities.setCapability("browserName", browserName);
         capabilities.setCapability("chromedriverDisableBuildCheck", "true");
+
+        // Capabilities for Android Native tests on EPAM Mobile Cloud
+        capabilities.setCapability("appPackage", appPackage);
+        capabilities.setCapability("appActivity", appActivity);
+
+        // Capabilities for iOS Native tests on EPAM Mobile Cloud
+        capabilities.setCapability("bundleId", bundleId);
+        if (platformName.equals("iOS")) {
+            capabilities.setCapability("automationName", "XCUITest");
+        }
 
         try {
             appiumDriver = new AppiumDriver(new URL(System.getProperty("ts.appium")), capabilities);
